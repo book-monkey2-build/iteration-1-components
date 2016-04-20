14363dafbcb9aa8dd73eb8e32ae1fac17d1335e9
The Buildbot <buildbot@angular2buch.de>
2016-04-20 14:42:41 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/0f434512e7f1978ee66177c12401e406fbb10dc8
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/124491593

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 var browser_1 = require('angular2/platform/browser');
 var book_monkey_1 = require('./app/book-monkey');
-browser_1.bootstrap(book_monkey_1.BookMonkeyApp, []);
+var router_1 = require('angular2/router');
+browser_1.bootstrap(book_monkey_1.BookMonkeyApp, [
+    router_1.ROUTER_PROVIDERS
+]);
 //# sourceMappingURL=app.js.map
