3cc25080318d3bc1fa1514cbbe7e5f42e5bf1b00
The Buildbot <buildbot@angular2buch.de>
2016-05-01 20:39:28 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/1aa9a24bb7c018c8b94884a8cbac2a8d2ddb83bb
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/127083800

--- app/book-monkey.js
+++ app/book-monkey.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './book-list/book-list.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './book-list/index'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,15 +10,15 @@ System.register(['angular2/core', './book-list/book-list.component'], function(e
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, book_list_component_1;
+    var core_1, index_1;
     var BookMonkeyApp;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (book_list_component_1_1) {
-                book_list_component_1 = book_list_component_1_1;
+            function (index_1_1) {
+                index_1 = index_1_1;
             }],
         execute: function() {
             BookMonkeyApp = (function () {
@@ -27,7 +27,7 @@ System.register(['angular2/core', './book-list/book-list.component'], function(e
                 BookMonkeyApp = __decorate([
                     core_1.Component({
                         selector: 'book-monkey-app',
-                        directives: [book_list_component_1.BookListComponent],
+                        directives: [index_1.BookListComponent],
                         template: "<book-list></book-list>"
                     }), 
                     __metadata('design:paramtypes', [])
