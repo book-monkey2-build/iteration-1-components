1e3a491792209edfa09655d51073b16ce8266157
The Buildbot <buildbot@angular2buch.de>
2016-07-21 16:17:42 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/ba5597ffca8d6d81d2ad2fade12feb1ad9cb6e2e
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/146408041

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var book_1 = require('../domain/book');
+var book_1 = require('../shared/book');
 var BookListComponent = (function () {
     function BookListComponent() {
         this.books = [
