f81c3d5d8e64919fb44eb5b0b473392190aa4ef0
The Buildbot <buildbot@angular2buch.de>
2016-08-04 14:13:14 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/979ced22f02f1c308b8e5a659079c541909e583a
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/149768230

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -12,11 +12,13 @@ var core_1 = require('@angular/core');
 var book_1 = require('../shared/book');
 var BookListComponent = (function () {
     function BookListComponent() {
+    }
+    BookListComponent.prototype.ngOnInit = function () {
         this.books = [
             new book_1.Book('9783864903571', 'Angular 2', ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'], new Date(2016, 5, 26), 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen', 5, [new book_1.Thumbnail('http://goo.gl/nDi0Fc', 'Buchcover')], 'Dieses Buch vermittelt einen Schnelleinstieg in Angular 2...'),
             new book_1.Book('9783864901546', 'AngularJS', ['Philipp Tarasiewicz', 'Robin Böhm'], new Date(2014, 5, 29), 'Eine praktische Einführung', 5, [new book_1.Thumbnail('https://goo.gl/Y5lFVE', 'Buchcover')], 'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts...')
         ];
-    }
+    };
     BookListComponent = __decorate([
         core_1.Component({
             selector: 'book-list',
